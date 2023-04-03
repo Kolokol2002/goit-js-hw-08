caf5a08606856fd820a2fc871fea5315009f7e5d
Maksym Karalash <Maks.karalash@gmail.com>
2023-04-03 19:12:02 +0300

    task-1 DONE

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const imageMarkup = galleryItems.map(({ preview, original, description }) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}" title="${description}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionSelector: 'self',
+    captionDelay: 250,
+});
+lightbox.on('show.simplelightbox')
+
+galleryEl.addEventListener('click', onOpenModalImg)
+function onOpenModalImg(e) {
+    if (e.target.classList[0] !== 'gallery__image') {
+      return;
+    }
+    e.preventDefault()
+}
