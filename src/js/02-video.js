ad2cccf55ba415cd0860a47f1215f3f183ac3f3b
Maksym Karalash <Maks.karalash@gmail.com>
2023-04-05 19:04:44 +0300

    task-1 DONE

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+import * as LocalStorage from './local-storage';
+import * as throttle from 'lodash.throttle';
+import * as debounce from 'lodash.debounce';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTimeVideo =
+  LocalStorage.get('videoplayer-current-time') === null
+    ? 0
+    : LocalStorage.get('videoplayer-current-time');
+
+player.setCurrentTime(currentTimeVideo);
+
+const currentVolumeVideo =
+  LocalStorage.get('videoplayer-current-volume') === null
+    ? 1
+    : LocalStorage.get('videoplayer-current-volume');
+
+player.setVolume(currentVolumeVideo);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    LocalStorage.save('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player.on(
+  'volumechange',
+  debounce(data => {
+    LocalStorage.save('videoplayer-current-volume', data.volume);
+    // console.log(data.volume);
+  }, 300)
+);
